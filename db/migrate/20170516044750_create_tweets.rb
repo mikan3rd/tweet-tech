201dd660aeb83937e473f59fd45b5b9122fce1a7
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-05-16 14:05:13 +0900

    tableの設定

--- db/migrate/20170516044750_create_tweets.rb
+++ db/migrate/20170516044750_create_tweets.rb
@@ -1,7 +1,9 @@
 class CreateTweets < ActiveRecord::Migration[5.0]
   def change
     create_table :tweets do |t|
-
+      t.string :content, null: false, limit: 140
+      t.string :image
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
