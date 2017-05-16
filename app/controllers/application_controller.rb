a14cc71512d5ff245f1559c640d660e102f8c892
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-05-16 17:15:43 +0900

    ログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:ninkname, :username])
+  end
+
 end
