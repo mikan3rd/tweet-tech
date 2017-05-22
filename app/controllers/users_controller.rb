a035928e3c88259ef5ffb5fee355b0701479fe8f
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-05-22 15:15:59 +0900

    いいねされた総数を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @tweets = @user.tweets.order(created_at: :desc)
+    @liked = 0
+    @tweets.each do |tweet|
+      @liked += tweet.likes.count
+    end
   end
 
   def search
