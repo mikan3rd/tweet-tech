b30d5ea920521b4665047027fc2366425b022bac
mikan3rd <mikan.no.kikoudhi@gmail.com>
2017-05-23 19:02:51 +0900

    likes_controllerの整理

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,27 +1,28 @@
 class LikesController < ApplicationController
+  before_action :set_variables
 
   def like
-    @tweet = Tweet.find(params[:tweet_id])
-    # binding.pry
-    @id_name = "#like-link-#{@tweet.id}"
-    @id_heart = "#heart-#{@tweet.id}"
     like = current_user.likes.new(tweet_id: @tweet.id)
     if like.save
-      # redirect_to :back, notice: "いいね！しました"
     else
       redirect_to :back, alert: "いいね！に失敗しました"
     end
   end
 
   def unlike
-    @tweet = Tweet.find(params[:tweet_id])
-    @id_name = "#like-link-#{@tweet.id}"
-    @id_heart = "#heart-#{@tweet.id}"
     like = current_user.likes.find_by(tweet_id: @tweet.id)
     if like.destroy
-      # redirect_to :back, notice: "いいね！を削除しました"
     else
       redirect_to :back, alert: "いいね！削除に失敗しました"
     end
   end
+
+  private
+
+  def set_variables
+    @tweet = Tweet.find(params[:tweet_id])
+    @id_name = "#like-link-#{@tweet.id}"
+    @id_heart = "#heart-#{@tweet.id}"
+  end
+
 end
